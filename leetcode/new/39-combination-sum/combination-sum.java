64d2378a885c06a6c78ff38fe4721bf51be847e4
SHLOK SINGH <157417369+buildwithshlok@users.noreply.github.com>
2026-08-01 18:22:29 +0530

    Time: 2 ms (96.35%) | Memory: 45.7 MB (68.67%) - LeetSync

--- leetcode/new/39-combination-sum/combination-sum.java
+++ leetcode/new/39-combination-sum/combination-sum.java
@@ -1,29 +1,24 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] nums, int target) {
         List<List<Integer>> result=new ArrayList<>();
-        List<Integer> current=new ArrayList<>();
-        com(0,result,current,nums,target);
+        List<Integer> curr=new ArrayList<>();
+        comb(0,target,result,curr,nums);
         return result;
-
         
     }
-    void com(int i,List<List<Integer>> result,List<Integer> current ,int [] nums,int target){
+  void comb(int i,int target,List<List<Integer>> result,List<Integer> curr,int []nums){
+    if(target==0){
+        result.add(new ArrayList<>(curr));
+        return;
+    }
+    if(i==nums.length) return ;
+    if(target>=nums[i]){
+        curr.add(nums[i]);
+        comb(i,target-nums[i],result,curr,nums);
+        curr.remove(curr.size()-1);
+    }
+        comb(i+1,target,result,curr,nums);
+    }
+
     
-        if(i==nums.length){
-        if(target==0) result.add(new ArrayList<>(current));
-        return ;}
-        //take 
-        if(target>=nums[i]){
-            current.add(nums[i]);
-            com(i,result,current,nums,target-nums[i]);
-            current.remove(current.size()-1);
-        }
-        com(i+1,result,current,nums,target);
-        
-
-
-        //not take
-
-
-    }
 }
