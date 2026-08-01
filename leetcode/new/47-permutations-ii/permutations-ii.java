8eb2fb51c2ce5a25100f26877d9e5ed56f185925
SHLOK SINGH <157417369+buildwithshlok@users.noreply.github.com>
2026-08-01 20:54:22 +0530

    Time: 716 ms (5.15%) | Memory: 46.7 MB (60.15%) - LeetSync

--- leetcode/new/47-permutations-ii/permutations-ii.java
+++ leetcode/new/47-permutations-ii/permutations-ii.java
@@ -10,12 +10,13 @@ class Solution {
 
     }
     void perm(int nums[],List<List<Integer>> result,List<Integer> curr,boolean vis[]){
-        if(curr.size()==nums.length){
+        if(curr.size()==nums.length && !result.contains(curr)){
             result.add(new ArrayList<>(curr)); return ;
         }
+      //  if(curr.size()==nums.length) return ;
         for(int i=0;i<nums.length;i++){
             if(vis[i]) continue;
-            if(i>0 && nums[i]==nums[i-1] && !vis[i-1]) continue;
+            // if(i>0 && nums[i]==nums[i-1] && !vis[i-1]) continue;
             curr.add(nums[i]);
             vis[i]=true;
             perm(nums,result,curr,vis);
