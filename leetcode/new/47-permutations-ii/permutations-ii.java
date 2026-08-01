cf77ef2840b8c51e8c7cbfbf0eb3f7cc309ef076
SHLOK SINGH <157417369+buildwithshlok@users.noreply.github.com>
2026-08-01 20:28:51 +0530

    Time: 3 ms (91.25%) | Memory: 47 MB (19.66%) - LeetSync

---
+++ leetcode/new/47-permutations-ii/permutations-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result=new ArrayList<>();
+        List<Integer> curr=new ArrayList<>();
+        boolean []vis=new boolean[nums.length];
+        perm(nums,result,curr,vis);
+        return result;
+
+
+    }
+    void perm(int nums[],List<List<Integer>> result,List<Integer> curr,boolean vis[]){
+        if(curr.size()==nums.length){
+            result.add(new ArrayList<>(curr)); return ;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(vis[i]) continue;
+            if(i>0 && nums[i]==nums[i-1] && !vis[i-1]) continue;
+            curr.add(nums[i]);
+            vis[i]=true;
+            perm(nums,result,curr,vis);
+            curr.remove(curr.size()-1);
+            vis[i]=false;
+
+
+        }
+    }
+}
